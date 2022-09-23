c54d1a7c7fcfd769f606610a6b53418cc4432976
RasibNadeem175 <rasibnadeem101@gmail.com>
2022-09-23 23:24:31 +0500

    Made an insert function that handles how a value >256 is inserted

--- Register.py
+++ Register.py
@@ -1,4 +1,25 @@
 class Register:
-    storedBytes  = 0
-    def __init__():
-        storedBytes = bytearray(2)
+    storedBytes = bytearray(2)
+    binary = 0
+
+    def __init__(self):
+        self.storedBytes = bytearray(2)
+
+    def value(self):
+        return (self.storedBytes[0] << 4) + self.storedBytes[1]
+
+
+    #when given a 16 bit value insert in the register
+    def insert(self, value):
+        if( value >= 256):
+            self.storedBytes[0] = value & 0b1111
+            self.storedBytes[1] = value >> 4
+
+
+        else:
+            self.storedBytes[0] = 0
+            self.storedBytes[1] = value
+
+    def binary(self):
+        return bin(self.value())
+
