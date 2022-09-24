f23fc8b460ac9ddc15f1220f3dae8cd2d45d0865
RasibNadeem175 <rasibnadeem101@gmail.com>
2022-09-24 20:07:10 +0500

    remove whitespaces

--- operation_dict.py
+++ operation_dict.py
@@ -4,10 +4,8 @@ operations_list = {
             16: mov,
             17: add,
             18: sub,
-
             #19:	mul,
             #20:	div,
-
             30: movi,
             31: addi,
             32: subi,
