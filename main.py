f8d93e9c19a57322cd1b4adf46e5149fadba2ad8
RasibNadeem175 <rasibnadeem101@gmail.com>
2022-09-25 20:28:46 +0500

    main edited

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from main_functions import  decode, displayMemory, writeInMemory,  execute
 
 startingIndex = 0
-writeInMemory('p1.txt', startingIndex) #write the byte stream in memory starting from startingIndex and sets pc to startingIndex
+writeInMemory('p1-test.txt', startingIndex) #write the byte stream in memory starting from startingIndex and sets pc to startingIndex
 
 #the execution loop
 def run():
     count = 0
-    while(True):
+    Running = True
+    while(Running):
         print("Instruction Number", count)
-
+        #decode the first byte
         opCode = decode()
+        #execute the instruction of opcode
         Interrupt = execute(opCode)
-        
+        #
         if(Interrupt):  
             print(Interrupt)
-            break;
-        #print(stack)
+            Running = False
+
         displayMemory()
         count += 1
         
